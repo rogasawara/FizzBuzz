bf6d496d7e9a6237f35aa800f43fb2f627a17880
ogasawara <b073k060n@gmail.com>
2012-01-26 23:45:45 +0900

    edit it

--- GetPrimeNum.rb
+++ GetPrimeNum.rb
@@ -1,14 +1,14 @@
 puts 1
 (2..100).each do |num|
-			(2..num-1).each do |cand|
-				case num % cand
-					when 0
-						puts num
-						break
-					else
-					if(cand == num-1)
-						puts "prime number"
-					end
-				end
+	(2..num-1).each do |cand|
+		case num % cand
+		when 0
+			puts num
+			break
+		else
+			if(cand == num-1)
+				puts "prime number"
 			end
+		end
+	end
 end
