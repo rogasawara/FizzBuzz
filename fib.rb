f7bc6c090c48c5f4281ede94ec8d214a29fadd4c
ogasawara <b073k060n@gmail.com>
2012-01-27 21:29:55 +0900

    edit fib.rb

--- fib.rb
+++ fib.rb
@@ -1,8 +1,8 @@
 class Hoge
   @@ary=[] #into fib
-  #フィボナッチ数列生成
+  #(num)番目のフィボナッチ数列生成
   def fi(num)
-  #f(1)=0,f(2)=1
+  #fi(1)=0,fi(2)=1
    case num
     when 1
      @@ary << 0
@@ -13,11 +13,10 @@ class Hoge
     else
      @@ary << @@ary[num-3]+@@ary[num-2]
      return @@ary[num-3]+@@ary[num-2]
-     #return fi(num-2)+fi(num-1)
     end
  end
 end
-
+#100個つくってみた
 a= Hoge.new
 (1..100).each do |i|
  puts a.fi(i)
