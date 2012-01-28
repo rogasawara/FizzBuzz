cb78cf086cb900e03a1d0f507ab4171a0b6ba8e9
ogasawara <b073k060n@gmail.com>
2012-01-28 11:35:03 +0900

    edit fib.rb

--- fib.rb
+++ fib.rb
@@ -1,8 +1,9 @@
 class Hoge
-  @@ary=[] #into fib
+  @@ary=[] #数列格納
   #(num)番目のフィボナッチ数列生成
   def fi(num)
   #fi(1)=0,fi(2)=1
+   begin
    case num
     when 1
      @@ary << 0
@@ -11,13 +12,26 @@ class Hoge
      @@ary << 1
      return 1
     else
+    #あくまで配列から拾ってきますので直接アレだとアレ
      @@ary << @@ary[num-3]+@@ary[num-2]
      return @@ary[num-3]+@@ary[num-2]
+    #下のようにすると爆発する
+    #return fi(num-2)+fi(num-1)
     end
+    rescue
+     puts "えらああああああ 直接読んじゃだめ"
+    end
+  end
+   #1からlast番目のフィボナッチつくろう
+ def ran(last)
+  (1..last).each do |i|
+   puts fi(i)
+  end
  end
 end
-#100個つくってみた
+
+#10個つくってみた
 a= Hoge.new
-(1..100).each do |i|
- puts a.fi(i)
-end
+a.ran(10)
+p a.fi(100)
+
